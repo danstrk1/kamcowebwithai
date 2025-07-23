7ef0dbaf7fb40c431cf6dd2f6dc3b34df86f1218
danstrk1 <254makufulegend@gmail.com>
2025-07-23 11:17:35 +0300

    Update staff member images

---
+++ src/components/StaffSection.tsx
@@ -0,0 +1,18 @@
+export default function StaffSection() {
+  const staffMembers = [
+    {
+      name: "Mr. Wycliffe Wafula",
+      position: "Chief Principal",
+      image: "https://ext.same-assets.com/3024875690/791474338.jpeg",
+      description: "Our College is committed to delivering a broad and balanced curriculum which celebrates the full range of talents and skills of all our students.",
+      link: "/chief-principal"
+    },
+    {
+      name: "Mr. Fred Ngatia",
+      position: "B.O.M Chairman",
+      image: "https://ext.same-assets.com/3024875690/3177653016.jpeg",
+      description: "We feel greatly indebted to the government for providing the necessary financial support to pay the non-teaching staff and for providing teachers",
+      link: "/chairman-bom"
+    }
+  ];
+}
